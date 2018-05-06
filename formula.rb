04515a018c26aaac32c79f687658a24d805fd95d
atpons <taiga@atpons.com>
2018-05-06 15:24:24 +0900

    init

---
+++ formula.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Foo < Formula
+  url "https://github.com/atpons/pdmk/archive/v1.0.tar.gz"
+  homepage "https://github.com/atpons/pdmk"
+  sha1 "2c89a0d11a5b4885729e0388dc3845f9a42a2431"
+
+  def install
+    bin.install "pdmk"
+  end
+
+  def caveats
+    msg = <<-EOF.undent
+      If you use pdmk template directory (common), you need to make directory at ~/.pdmk/template
+    EOF
+  end
+
+end
